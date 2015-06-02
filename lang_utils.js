17c426abee8b3f71ee9cc4e3687a74973e0dd125
Tim Daubenschütz <tim@ascribe.io>
2015-06-02 12:00:59 +0200

    include lodash.templates

--- lang_utils.js
+++ lang_utils.js
@@ -1,12 +1,17 @@
 import languages from '../constants/languages';
+import template from 'lodash.template';
 
-let getText = function(s) {
+let getText = function(s, ...args) {
     let lang = navigator.language || navigator.userLanguage;
-    if(lang in languages && s in languages[lang]) {
-        return languages[lang][s];
-    } else {
-        throw new Error('Your language is not supported.');
-        // How ironic that this error is thrown in the english language...
+    try {
+        if(lang in languages) {
+            return languages[lang][s];
+        } else {
+            // just use the english language
+            return languages['en-US'][s];
+        }
+    } catch(err) {
+        console.error(err);
     }
 };
 
