78d7a685358674b736822ccfed70c131786cbf27
Tim Daubenschütz <tim@ascribe.io>
2015-06-02 14:29:06 +0200

    minor stuff

--- lang_utils.js
+++ lang_utils.js
@@ -4,6 +4,8 @@ import { formatText } from './general_utils';
 
 export function getLangText(s, ...args) {
     let lang = navigator.language || navigator.userLanguage;
+    // this is just for testing, as changing the navigator.language wasn't possible
+    //lang = 'de';
     try {
         if(lang in languages) {
             return formatText(languages[lang][s], args);
