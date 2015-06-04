1864693545d3fdbe0b303bdce5e54dcddc76aaec
Tim Daubenschütz <tim@ascribe.io>
2015-06-04 10:11:18 +0200

    add select all functionality for edition list table

--- lang_utils.js
+++ lang_utils.js
@@ -11,7 +11,7 @@ import { formatText } from './general_utils';
 export function getLangText(s, ...args) {
     let lang = navigator.language || navigator.userLanguage;
     // this is just for testing, as changing the navigator.language wasn't possible
-    lang = 'de';
+    //lang = 'de';
     try {
         if(lang in languages) {
             return formatText(languages[lang][s], args);
