b3e745d2976e1cfd52d07a00719ee5e821dd467f
Tim Daubenschütz <tim@ascribe.io>
2015-07-01 16:19:02 +0200

    fix bug in mergeOptions method

--- general_utils.js
+++ general_utils.js
@@ -64,6 +64,26 @@ export function formatText() {
     });
 }
 
+/*
+    Checks a list of objects for key duplicates and returns a boolean
+ */
+function _doesObjectListHaveDuplicates(l) {
+    let mergedList = [];
+
+    l = l.map((obj) => Object.keys(obj));
+
+    // Taken from: http://stackoverflow.com/a/10865042
+    // How to flatten an array of arrays in javascript.
+    // If two objects contain the same key, then these two keys 
+    // will actually be represented in the merged array
+    mergedList = mergedList.concat.apply(mergedList, l);
+
+    // Taken from: http://stackoverflow.com/a/7376645/1263876
+    // By casting the array to a set, and then checking if the size of the array
+    // shrunk in the process of casting, we can check if there were any duplicates
+    return (new Set(mergedList)).size !== mergedList.length;
+}
+
 /**
  * Takes a list of object and merges their keys to one object.
  * Uses mergeOptions for two objects.
@@ -71,6 +91,12 @@ export function formatText() {
  * @return {[type]}   [description]
  */
 export function mergeOptions(...l) {
+    // If the objects submitted in the list have duplicates,in their key names,
+    // abort the merge and tell the function's user to check his objects.
+    if(_doesObjectListHaveDuplicates(l)) {
+        throw new Error('The objects you submitted for merging have duplicates. Merge aborted.');
+    }
+
     let newObj = {};
 
     for(let i = 1; i < l.length; i++) {
@@ -86,18 +112,14 @@ export function mergeOptions(...l) {
  * @returns obj3 a new object based on obj1 and obj2
  * Taken from: http://stackoverflow.com/a/171256/1263876
  */
-function _mergeOptions(obj1, obj2){
+function _mergeOptions(obj1, obj2) {
     let obj3 = {};
     
     for (let attrname in obj1) {
         obj3[attrname] = obj1[attrname];
     }
     for (let attrname in obj2) {
-        if(attrname in obj3) {
-            throw Error('Overwrite Conflict: You\'re merging two objects with the same keys: ' + attrname);
-        } else {
-            obj3[attrname] = obj2[attrname];
-        }
+        obj3[attrname] = obj2[attrname];
     }
     return obj3;
 }
