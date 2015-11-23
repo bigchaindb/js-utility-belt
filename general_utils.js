f928de9f8858c02a6da8279fbb43eb41c3942279
Brett Sun <brett@ascribe.io>
2015-11-23 17:12:47 +0100

    Add functionality to match known errors
    Implemented as part of AD-1360 and AD-1378 as a way to match an error
    situation to a known class of errors so that we can modify behaviour or
    show pretty text to the user.

--- general_utils.js
+++ general_utils.js
@@ -179,6 +179,47 @@ export function omitFromObject(obj, filter) {
 }
 
 /**
+ * Recursively tests an object against a "match" object to see if the
+ * object is similar to the "match" object. In other words, this will
+ * deeply traverse the "match" object's properties and check them
+ * against the object by using the testFn.
+ *
+ * The object is considered a match if all primitive properties in the
+ * "match" object are found and accepted in the object by the testFn.
+ *
+ * @param  {object}     obj    Object to test
+ * @param  {object}     match  "Match" object to test against
+ * @param  {(function)} testFn Function to use on each property test.
+ *                             Return true to accept the match.
+ *                             By default, applies strict equality using ===
+ * @return {boolean}           True if obj matches the "match" object
+ */
+export function deepMatchObject(obj, match, testFn) {
+    if (typeof match !== 'object') {
+        throw new Error('Your specified match argument was not an object');
+    }
+
+    if (typeof testFn !== 'function') {
+        testFn = (objProp, matchProp) => {
+            return objProp === matchProp;
+        };
+    }
+
+    return Object
+            .keys(match)
+            .reduce((result, matchKey) => {
+                if (!result) { return false; }
+
+                const objProp = obj[matchKey];
+                const matchProp = match[matchKey];
+
+                return (typeof matchProp === 'object') ? testObjAgainstMatch(objProp, matchProp, testFn)
+                                                       : testFn(objProp, matchProp);
+
+            }, true);
+}
+
+/**
  * Takes a string and breaks it at the supplied index and replaces it
  * with a (potentially) short string that also has been provided
  * @param  {string} text        The string to truncate
