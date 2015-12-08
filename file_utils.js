73b44aa8354d775e1f1bde573f365a5a09b12d52
Brett Sun <brett@ascribe.io>
2015-12-08 16:11:27 +0100

    Merge with master

--- file_utils.js
+++ file_utils.js
@@ -85,3 +85,20 @@ export function computeHashOfFile(file) {
         loadNext();
     });
 }
+
+/**
+ * Extracts a file extension from a string, by splitting by dot and taking
+ * the last substring
+ *
+ * If a file without an extension is submitted (file), then
+ * this method just returns an empty string.
+ * @param  {string} s file's name + extension
+ * @return {string}   file extension
+ *
+ * Via: http://stackoverflow.com/a/190878/1263876
+ */
+export function extractFileExtensionFromString(s) {
+    const explodedFileName = s.split('.');
+    return explodedFileName.length > 1 ? explodedFileName.pop()
+                                       : '';
+}
