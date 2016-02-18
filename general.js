8ca288397c01f21db81af18f25c000c9688b29f6
Brett Sun <qisheng.brett.sun@gmail.com>
2016-02-18 17:40:54 +0100

    Add arrayFrom to handle cases where Array.from() doesn't work

--- general.js
+++ general.js
@@ -243,6 +243,22 @@ export function truncateTextAtCharIndex(text, truncIndex, replacement = '...') {
     return text.length > truncIndex ? (text.slice(0, truncIndex) + replacement) : text;
 }
 
+
+/**
+ * Dumb shim to convert array-like data structures to an iterable format (ie. array)
+ * Apparently Array.from() doesn't cover some cases (ie. FileLists)
+ *
+ * @param  {object} arrayLike Array like object
+ * @return {any[]}            Actual array containing the same objects in arrayLike
+ */
+export function arrayFrom(arrayLike) {
+    const array = [];
+
+    Array.prototype.forEach.call(arrayLike, (item) => array.push(item));
+
+    return array;
+}
+
 /**
  * Takes two lists and returns their intersection as a list
  * @param  {Array} a
