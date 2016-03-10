edb7bc88aade6e2973271181ff9c26c5cbf705cf
Tim Daubenschütz <tim@ascribe.io>
2016-03-10 15:09:22 +0100

    Restrict thumbnail expansion if file is TIFF

--- file_utils.js
+++ file_utils.js
@@ -102,3 +102,17 @@ export function extractFileExtensionFromString(s) {
     return explodedFileName.length > 1 ? explodedFileName.pop()
                                        : '';
 }
+
+
+/**
+ * Extracts a file extension from a url.
+ *
+ * If a file without an extension is submitted (file), then
+ * this method just returns an empty string.
+ * @param  {string}     url A url ending in a file
+ * @return {string}     a file extension
+ */
+export function extractFileExtensionFromUrl(url) {
+    const fileName = url.split('/').pop();
+    return extractFileExtensionFromString(fileName);
+}
