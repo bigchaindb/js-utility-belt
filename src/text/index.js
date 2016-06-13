df5a9b3994fab9120b524480e2858d7e15a42580
Brett Sun <qisheng.brett.sun@gmail.com>
2016-06-13 16:34:24 +0200

    Fix typo when exporting truncateText

--- src/text/index.js
+++ src/text/index.js
@@ -1,4 +1,4 @@
 // Ignore error as we may have more exports in the future
 export formatText from './format_text';
 export sprintf from './sprintf';
-export trucateText from './truncate_text';
+export truncateText from './truncate_text';
